a46c3bc46417a3b6ecb3bfa551e319e91057f7cf
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-03-29 20:49:47 -0400

    grade book in progress

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,53 +1,127 @@
 package io.zipcoder;
 
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Classroom {
-    int numberOfStudents = 30;
-    Student[] students;
+    private int numberOfStudents;
+    private Student[] students;
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
 
     public Classroom() {
-
+        this.students = new Student[30];
     }
 
     public Classroom(int numberOfStudents) {
-        this.numberOfStudents = numberOfStudents;
+        this.students = new Student[numberOfStudents];
     }
 
     public Classroom(Student[] students) {
-        this.students = students;
-    }
-    public Classroom(int numberOfStudents, Student[] student) {
-        this.numberOfStudents = numberOfStudents;
-        this.students = student;
+        this.students = new Student[30];
+        for (int element = 0; element < students.length; element++) {
+            this.students[element] = students[element];
+        }
     }
 
-    public Student[] getStudents(){
+    public Student[] getStudents() {
         return this.students;
     }
 
-    public double getAverageExamScore(){
-        double sumOfScores = 0.0;
-        for (int i = 0; i < students.length; i++) {
-            double num = students[i].getAverageExamScore();
-            sumOfScores += num;
-        }
-        return sumOfScores/students.length;
+    public Map<Student, String> getGradeBook(){
+        Map<Student, String> graded = new HashMap<>();
+        Student[] student = getStudents();
+
+        for (int studentIndex = 0; studentIndex < getNumberOfStudents(); studentIndex++){
+            //Math.max()
+
+            if (student[studentIndex] == null) {
+                return graded;
+            }//check fro null
+            if (student[studentIndex].examScores.get(0) > 91.0 || student[studentIndex].examScores.get(0) <= 100) {
+                graded.put(student[studentIndex], "A");
+            } else if (student[studentIndex].examScores.get(0) > 80 || student[studentIndex].examScores.get(0) <= 91) {
+                graded.put(student[studentIndex], "B");
+            } else if (student[studentIndex].examScores.get(0) > 70 || student[studentIndex].examScores.get(0) <= 80) {
+                graded.put(student[studentIndex], "C");
+            } else if (student[studentIndex].examScores.get(0) > 60 || student[studentIndex].examScores.get(0) <= 70) {
+                graded.put(student[studentIndex], "D");
+            } else if (student[studentIndex].examScores.get(0) > 50 || student[studentIndex].examScores.get(0) <= 60) {
+                graded.put(student[studentIndex], "E");
+            } else {
+                graded.put(student[studentIndex], "F");
+            }
+            }
+        return graded;
     }
 
-    public Student[] addStudent(Student student){
-        for (int i = 0; i < students.length; i++) {
-            System.out.println(students.length);
-            if(students[i]== null){
-                students[i] = student;
+    public double getAverageExamScore() {
+        double sumOfScores = 0.0;
+        double count = 0.0;
+        for (int i = 0; i < this.students.length; i++) {
+            if (this.students[i] != null){
+                count++;
+                double num = this.students[i].getAverageExamScore();
+                sumOfScores += num;
+            }
+
+        }
+        return sumOfScores / count;
+    }
+
+    public Student[] addStudent(Student student) {
+        for (int i = 0; i < this.students.length; i++) {
+            if (this.students[i] == null) {
+                this.students[i] = student;
+                numberOfStudents++;
+                break;
             }
         }
-        return students;
+        return this.students;
+    }
+
+    public void removeStudent(String firstName, String lastName) {
+        int nullStudentIndex = -1;
+        for (int indexOfStudent = 0; indexOfStudent < students.length; indexOfStudent++) {
+            if (this.students[indexOfStudent] != null && Objects.equals(students[indexOfStudent].firstName, firstName) && Objects.equals(students[indexOfStudent].lastName, lastName)) {
+                nullStudentIndex = indexOfStudent;
+                students[indexOfStudent] = null;
+            }
+        }
+        if (nullStudentIndex >= 0) {
+            for (int indexOfStudent = nullStudentIndex; indexOfStudent < students.length - 1; indexOfStudent++) {
+                Student holder = students[indexOfStudent];
+                students[indexOfStudent] = students[indexOfStudent + 1];
+                students[indexOfStudent + 1] = holder;
+            }
+        }
+    }
+
+    public Student[] getStudentsByScore() {
+        Student[] studentArrayByScore = new Student[students.length];
+        for (int indexOfStudent = 0; indexOfStudent < students.length; indexOfStudent++) {
+            double measure = 0.0;
+            double count = 0.0;
+            if (this.students[indexOfStudent] != null){
+                if (measure < this.students[indexOfStudent].getExamScores().get(0)) {
+                    measure = this.students[indexOfStudent].getExamScores().get(0);
+                    studentArrayByScore[indexOfStudent] = this.students[indexOfStudent];
+                } else {
+                    studentArrayByScore[studentArrayByScore.length] = this.students[indexOfStudent];
+                }
+            }
+
+        }
+        return studentArrayByScore;
+
     }
 
     public static void main(String[] args) {
         Classroom classroom = new Classroom();
-        System.out.println(Arrays.toString(classroom.getStudents()));
+       // System.out.println(Arrays.toString(classroom.getStudents()));
     }
+
+
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -31,7 +33,7 @@ public class ClassroomTest {
         double output = classroom.getAverageExamScore();
 
         // Then
-        Assert.assertEquals(expected, output, 003);
+        Assert.assertEquals(expected, output, 0);
     }
 
     @Test
@@ -48,8 +50,9 @@ public class ClassroomTest {
         Student s2 = new Student("student", "two", s2Scores);
         Student s3 = new Student("student", "three", s2Scores);
 
-        Student[] students = {s1, s2};
-
+        Student[] students = new Student[2];
+        students[0]= s1;
+        students[1]= s2;
         Classroom classroom = new Classroom(students);
         classroom.addStudent(s3);
 
@@ -58,67 +61,100 @@ public class ClassroomTest {
         Student[] output = classroom.getStudents();
 
         // Then
-        assertEquals(output[1],s3);
+        assertEquals(output[2],s3);
     }
 
-//    @Test
-//    public void setExamScoreTest() {
-//        // : Given
-//        String firstName = "Leon";
-//        String lastName = "Hunter";
-//        ArrayList<Double> examScores = new ArrayList<>();
-//        Student student = new Student(firstName, lastName, examScores);
-//        examScores.add(100.0);
-//        examScores.add(95.0);
-//        examScores.add(123.0);
-//
-//
-//        // When
-//        student.setExamScore(1, 150.0);
-//        ArrayList<Double> output = student.getExamScores();
-//
-//        // Then
-//        Assert.assertEquals(output, student.getExamScores());
-//    }
+    @Test
+    public void removeStudent_ShouldRemoveStudentAndReorderArray(){
+        ArrayList<Double> s1Scores = new ArrayList<>();
+        ArrayList<Double> s2Scores = new ArrayList<>();
+        s1Scores.add(100.0);
+        s1Scores.add(150.0);
+        s2Scores.add(225.0);
+        s2Scores.add(25.0);
 
-//    @Test
-//    public void getAverageExamScoreTest() {
-//        // : Given
-//        String firstName = "Leon";
-//        String lastName = "Hunter";
-//        ArrayList<Double> examScores = new ArrayList<>();
-//        Student student = new Student(firstName, lastName, examScores);
-//        examScores.add(100.0);
-//        examScores.add(150.0);
-//        examScores.add(250.0);
-//        examScores.add(0.0);
-//
-//        // When
-//        double expected = (125.0);
-//        double output = student.getAverageExamScore();
-//
-//        // Then
-//        Assert.assertEquals(expected, output, 0.003);
-//    }
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+        Student s3 = new Student("student", "three", s2Scores);
 
-//    @Test
-//    public void toStringTest(){
-//        // : Given
-//        String firstName = "Leon";
-//        String lastName = "Hunter";
-//        ArrayList<Double> examScores = new ArrayList<>();
-//        Student student = new Student(firstName, lastName, examScores);
-//        examScores.add(100.0);
-//        examScores.add(150.0);
-//        examScores.add(250.0);
-//        examScores.add(0.0);
-//
-//
-//        // When
-//        String output = student.toString();
-//
-//        // Then
-//        System.out.println(output);
-//        //return null;
-//    }
+        Classroom classroom = new Classroom();
+        classroom.addStudent(s3);
+        classroom.addStudent(s2);
+        classroom.addStudent(s1);
+
+        classroom.removeStudent(s2.firstName, s2.lastName);
+        Student[] students = classroom.getStudents();
+
+        Assert.assertNull(students[2]);
+        Assert.assertEquals(s1.lastName, students[1].lastName);
+
+
+    }
+
+    @Test
+    public void getStudentsByScore_ShouldReturnStudentArrayByScoreInOrder() {
+        ArrayList<Double> s1Scores = new ArrayList<>();
+        ArrayList<Double> s2Scores = new ArrayList<>();
+        ArrayList<Double> s3Scores = new ArrayList<>();
+        ArrayList<Double> s4Scores = new ArrayList<>();
+        s1Scores.add(100.0);
+        s2Scores.add(150.0);
+        s3Scores.add(225.0);
+        s4Scores.add(25.0);
+
+
+        Student s1 = new Student("Jimmy", "one", s1Scores);
+        Student s2 = new Student("John", "two", s2Scores);
+        Student s3 = new Student("Jomes", "three", s3Scores);
+        Student s4 = new Student("Julien", "four", s4Scores);
+        //When
+        Classroom classroom = new Classroom();
+        classroom.addStudent(s1);
+        classroom.addStudent(s2);
+        classroom.addStudent(s3);
+        classroom.addStudent(s4);
+
+        //Then
+        //Student expected = classroom.getStudents()[0];
+       //Student student = classroom.getStudentsByScore()[0];
+
+
+        Assert.assertEquals(s1, classroom.getStudentsByScore()[0]);
+    }
+
+    @Test
+    public void getGradeBook_TestToGetGradesFromStudentScores(){
+       //Given
+        ArrayList<Double> s1Scores = new ArrayList<>();
+        ArrayList<Double> s2Scores = new ArrayList<>();
+        ArrayList<Double> s3Scores = new ArrayList<>();
+        ArrayList<Double> s4Scores = new ArrayList<>();
+        s1Scores.add(100.0);
+        s2Scores.add(150.0);
+        s3Scores.add(225.0);
+        s4Scores.add(25.0);
+
+
+        Student s1 = new Student("Jimmy", "one", s1Scores);
+        Student s2 = new Student("John", "two", s2Scores);
+        Student s3 = new Student("Jones", "three", s3Scores);
+        Student s4 = new Student("Julien", "four", s4Scores);
+        System.out.println("here:  "+ s1.examScores.size());
+        Classroom classroom = new Classroom();
+        classroom.addStudent(s1);
+        classroom.addStudent(s2);
+        classroom.addStudent(s3);
+        classroom.addStudent(s4);
+
+
+        //When
+        Map<Student, String> student = classroom.getGradeBook();
+       // System.out.println(student.get(s1));
+
+
+        //Then
+        //Assert.assertEquals("A", classroom.getGradeBook().get(s1));
+        Assert.assertEquals(student.get(s1), classroom.getGradeBook().get(0));
+
+    }
 }
